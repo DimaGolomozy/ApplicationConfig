b5bd66d07293eac6663817393d796419c03766d8
dima <dima.golomozy@gmail.com>
2017-02-26 18:43:29 +0200

    updated annotation added map property annotation

--- src/main/java/com/dgol/applicationConfig/annotation/MapProperty.java
+++ src/main/java/com/dgol/applicationConfig/annotation/MapProperty.java
@@ -6,18 +6,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Collection;
+import java.util.Map;
 
 /**
- * Annotation for properties that need to be stored in a {@link Collection}
- * Basically if the value is 1,2,3,4, one can store them in a {@link java.util.HashSet}
- * by this annotation
+ * Annotation for properties that need to be stored in a {@link Map}
  *
  * @author dima.golomozy
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface CollectionProperty
+public @interface MapProperty
 {
     /**
      * @return true if need to update the key's value if the properties get changed
@@ -30,12 +28,22 @@ public @interface CollectionProperty
     String delimiter();
 
     /**
-     * @return the {@link Converter} to use to convert the key's value
+     * @return the delimiter to split the value to get the key-value pair
      */
-    Class<? extends Converter<?>> converter();
+    String keyValueDelimiter();
 
     /**
-     * @return the {@link Collection} class to create to store the converted values
+     * @return the {@link Converter} to use to convert the key
      */
-    Class<? extends Collection> collection();
+    Class<? extends Converter<?>> keyConverter();
+
+    /**
+     * @return the {@link Converter} to use to convert the value
+     */
+    Class<? extends Converter<?>> valueConverter();
+
+    /**
+     * @return the {@link Map} class to create to store the converted values
+     */
+    Class<? extends Map> map();
 }
--- src/main/java/com/dgol/applicationConfig/annotation/Property.java
+++ src/main/java/com/dgol/applicationConfig/annotation/Property.java
@@ -1,7 +1,7 @@
 package com.dgol.applicationConfig.annotation;
 
 import com.dgol.applicationConfig.converters.Converter;
-import com.dgol.applicationConfig.converters.primitives.StringConverter;
+import com.dgol.applicationConfig.converters.StringConverter;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,12 +9,22 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by dima.golomozy on 05/05/2016.
+ * Annotation for basic property;
+ * Integer, String, Double, custom converters, etc..
+ *
+ * @author dima.golomozy
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Property
 {
+    /**
+     * @return true if need to update the key's value if the properties get changed
+     */
     boolean putToUpdate() default false;
+
+    /**
+     * @return the {@link Converter} to use to convert the key's value
+     */
     Class<? extends Converter<?>> converter() default StringConverter.class;
 }
